b78a691db1799ff7f96a6098902d56fc2d221ef1
chaitanya <chaitanyaarora3@gmail.com>
2022-01-15 20:24:22 +0530

    updated jan redfin

--- ancient_X2.mk
+++ ancient_X2.mk
@@ -31,6 +31,7 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
 PRODUCT_MANUFACTURER := realme
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo 
